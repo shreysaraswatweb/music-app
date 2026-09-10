b1bd19b4bcc989d8cc2c660b6b32b7642404c2a3
Shrey Saraswat <shreysaraswat1996@gmail.com>
2026-09-10 18:30:00 +0530

    Update

---
+++ src/models/user.models.js
@@ -0,0 +1,27 @@
+const mongoose = require ('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email:{type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    role:{
+        type: String,
+        enum: ['user', 'artist'], //enum humko ye batata hai ki role ki value ya to user hogi ya artist hogi
+        default: 'user',
+    }
+})
+
+const userModel = mongoose.model("user", userSchema); 
+
+module.exports = userModel;
