fd52e6646dbd0e69d751ac57f957e72d62c8108f
Shrey Saraswat <shreysaraswat1996@gmail.com>
2026-09-12 14:00:00 +0530

    Login User

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -46,6 +46,50 @@ async function registerUser (req, res) {
     })
 }
 
+async function loginUser (req,res){
+
+    const {username, email, password} = req.body;
+    const user = await userModel.findOne({
+        $or: [
+            {username},
+            {email}
+        ]
+    })
+    if(!user){
+        return res.status(401).json({
+            message:"Invalid Credentials"
+        })
+    
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid){
+        return res.status(401).json ({
+            message: "Invalid Password"
+        })
+    }
+
+    const token = jwt.sign({
+        id : user._id,
+        role: user.role
+    }, process.env.JWT_SECRET)
+
+    res.cookie("token", token);
+
+    res.status(200).json({
+        messaage: "User Logged in successfully",
+        user:{
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role
+        }   
+    })
+}
+
+
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,4 +6,6 @@ const router = express.Router();
 
 router.post ('/register', authController.registerUser)
 
+router.post('/login', authController.loginUser)
+
 module.exports = router;
